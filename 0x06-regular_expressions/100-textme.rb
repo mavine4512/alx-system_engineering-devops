c43e0fbcb7eff4f9de512819a1ecffb6bc5fb98c
“mavine4512” <mavineoyugi@gmail.com>
2023-10-03 07:38:03 -0700

    fixed 100-textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # A regular expression that is matches a give pattern
-puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
+ptn = /(?:(?<=from:)(?:\p{L}+|\+?\d+)|(?<=to:)(?:\p{L}+|\+?\d+)|(?<=flags:)(?:-?\d+:?)*)/
+puts ARGV[0].scan(ptn).join(',')
