d997727eec588ecc160b5fb15ee0402f608ef1ae
“mavine4512” <mavineoyugi@gmail.com>
2024-01-02 08:54:13 -0800

    fixed pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
             done += 1
 
     print("Employee {} is done with tasks({}/{}):"
-            .format(employeeName, done, len(tasks)))
+          .format(employeeName, done, len(tasks)))
 
     for task in done_tasks:
         print("\t {}".format(task.get('title')))
